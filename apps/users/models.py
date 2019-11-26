d185ffb314f34c1ea6ea472a7c30af1972b5a611
timothyshen <timothyshen1@hotmail.com>
2019-11-26 22:18:02 +0000

    update

--- apps/users/models.py
+++ apps/users/models.py
@@ -5,28 +5,40 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser, PermissionsMixin, AbstractBaseUser
 from datetime import datetime
 from phonenumber_field.modelfields import PhoneNumberField
+from books.models import *
 
 
 class User(AbstractUser):
+    role_choice = (
+        ('Reader', u'Reader'),
+        ('Author', u'Author'),
+        ('Editor', u'Editor'),
+        ('Admin', u'Admin')
+    )
+    user_mobile = PhoneNumberField(null=True, unique=True, verbose_name='Mobile')
+    user_gender = models.CharField(choices=(("male", u"Male"), ("female", u"Female")),
+                                   default="Female",
+                                   max_length=150, verbose_name='Gender')
+    user_birthday = models.DateField(null=True, blank=True, verbose_name="Birthday")
+    user_icon = models.ImageField(upload_to="media/image/%Y/%m",
+                                  default=u"media/image/default.png",
+                                  max_length=1000,
+                                  verbose_name=u"User icon", null=True)
+    user_role = models.CharField(choices=role_choice,
+                                 max_length=150,
+                                 default='Admin',
+                                 verbose_name='Role')
 
-    user_mobile = PhoneNumberField(null = True, unique = True, verbose_name = 'Mobile')
-    user_gender = models.CharField(choices = (("male", u"Male"), ("female", u"Female")),
-                                   default = "Female",
-                                   max_length = 150, verbose_name = 'Gender')
-    user_birthday = models.DateField(null = True, blank = True, verbose_name = "Birthday")
-    user_icon = models.ImageField(upload_to = "media/image/%Y/%m",
-                                  default = u"media/image/default.png",
-                                  max_length = 1000,
-                                  verbose_name = u"User icon", null = True)
     def __str__(self):
         return self.username
+
+    def get_user_role_display(self):
+        return self.role_choice
 
     class Meta:
         verbose_name_plural = "user"
         verbose_name = verbose_name_plural
         db_table = 'User'
-
-
 
 
 class Admin(User):
@@ -42,13 +54,15 @@ class Admin(User):
 
 
 class Reader(models.Model):
-    user = models.ForeignKey(User, on_delete = models.CASCADE, related_name = 'user_reader')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_reader')
     is_user_vip = models.CharField(choices=(('normal', u'Normal'), ('vip', u'VIP')),
                                    default='Normal',
                                    max_length=10,
-                                   verbose_name=u'Vip status')
+                                   verbose_name=u'Vip status',
+                                   editable=False)
     vip_validate = models.DateField(verbose_name=u"Validate Until",
-                                    default='', null=True)
+                                    default='', null=True,
+                                    editable=False)
 
     def __str__(self):
         template = '{0.user.email} {0.user_mobile}'
@@ -61,9 +75,8 @@ class Reader(models.Model):
 
 
 class Author(models.Model):
-
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='user_author')
-
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_author')
+    contract_number = models.IntegerField(verbose_name= 'Contact number', null=True, blank=True, max_length=100)
     def __str__(self):
         return self.user.username
 
@@ -71,4 +84,3 @@ class Author(models.Model):
         verbose_name_plural = 'Author'
         verbose_name = verbose_name_plural
         db_table = 'Author'
-
