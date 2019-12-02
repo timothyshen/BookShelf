7fae62c62b37ce680459e16a0c9179e37915c793
Timothy <timothyshen1@hotmail.com>
2019-12-02 17:21:36 +0000

    Added book db

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -7,7 +7,7 @@ from django.contrib.auth.admin import UserAdmin
 from .models import Reader, User, Author, Admin
 
 
-class ReaderInlines(admin.TabularInline):
+class ReaderInlines(admin.StackedInline):
     model = Reader
     extra = 0
     readonly_fields = ['is_user_vip', 'vip_validate']
@@ -19,8 +19,18 @@ class AuthorInlines(admin.TabularInline):
     readonly_fields = ['contract_number']
 
 
-class ReaderAdmin(UserAdmin):
-    inlines = []
+class UserAdmin(UserAdmin):
+    model = User
 
-# admin.site.register(Reader, ReaderAdmin)
+    inlines = []
+    list_filter = ('user_role',)
+
+    date_hierarchy = 'date_joined'
+    ordering = ('date_joined',)
+
+    def get_inline_instances(self, request, obj=None):
+        if obj.user_role == 'Reader':
+            return ReaderInlines(self.model, self.admin_site)
+
+admin.site.register(User, UserAdmin)
 # admin.site.register(Admin)
--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,4 +2,19 @@ import axios from 'axios';
 
 let host = 'http://127.0.0.1:8000';
 
-export const ;
+export const login = params => {
+    return axios.post(`${host}/login/`, params)
+}
+
+
+export const register = parmas => {
+    return axios.post(`${host}/users/`, parmas)
+}
+
+export const getUserDetail = () => {
+    return axios.get(`${host}/users/1/`)
+}
+
+export const updateUserInfo = params => {
+    return axios.patch(`${host}/users/1/`, params)
+}
