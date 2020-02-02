d728d6818a4477b0c5432e6182e407c1e1cafd5f
timothyshen <timothyshen1@hotmail.com>
2020-02-02 17:03:07 +0000

    Added the top chart component draft to the page

---
+++ frontend/src/views/user_login/register.vue
@@ -0,0 +1,20 @@
+<template>
+  <div><!--最外层-->
+    <div>
+      <div>
+        <h2>Username:</h2>
+        <input type="text" ></input>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "register"
+    }
+</script>
+
+<style scoped>
+
+</style>
