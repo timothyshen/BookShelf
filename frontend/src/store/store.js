38f010edd0901ed0cdca4f0de11d35763978ba7f
timothyshen <timothyshen1@hotmail.com>
2020-02-13 17:16:42 +0000

    Added Vuex to mutate the cookie retreive process

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -2,3 +2,25 @@
 import Vue from 'vue'
 //implement vuex
 import Vuex from 'vuex';
+import cookie from "../static/cookie/cookie";
+import mutations from "./mutations";
+import * as actions from './actions';
+import * as getters from './getters';
+
+Vue.use(Vuex);
+
+const userInfo = {
+  name:cookie.getCookie('name'),
+  token:cookie.getCookie('token')
+};
+
+const state = {
+  userInfo
+}
+const store = new Vuex.Store({
+  state,
+  mutations,
+  actions,
+  getters
+});
+
